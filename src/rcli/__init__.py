9a11e7e83dcfde8106ec1935afd672d12753498a
rrenode <robert.renodej@gmail.com>
2025-04-28 09:55:51 -0400

    Added rCli class

--- src/rcli/__init__.py
+++ src/rcli/__init__.py
@@ -14,3 +14,29 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+from .registry import CommandRegistry
+from .commands import auto_import_subcommands
+from .parser import parse_args
+
+class rCli:
+    def __init__(self):
+        auto_import_subcommands("commands")
+        self.registry = CommandRegistry()
+        
+        self.__parse__()
+    
+    @property
+    def commands(self):
+        return self.registry.all_commands()
+    
+    @commands.setter
+    def commands(self, value):
+        raise Exception("Do not set the commands for rCli. Instead use rCli().register_command(handler, name) or alternitively register with rCli.CommandRegistry().register(handler, name)")
+    
+    def register_command(self, handler, name):
+        self.registry.register(handler, name)
+    
+    def __parse__(self):
+        self.raw_args = sys.argv
+        self.args = parse_args(self.raw_args)
